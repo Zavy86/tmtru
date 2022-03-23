5d842e0911d42173f444638283576b9f0ea616f6
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-03-23 14:48:09 +0100

    Update Bootstrap

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -6,7 +6,8 @@
  * @author Manuel Zavatta <manuel.zavatta@gmail.com>
  */
 error_reporting(E_ALL);
-ini_set('display_errors',(isset($_GET['debug']) && $_GET['debug']==1));
 define('DIR',str_replace(['/','\\'],DIRECTORY_SEPARATOR,__DIR__.'/'));
-if(version_compare(PHP_VERSION,'8.1.0')<0){die('Required at least PHP version 8.1.0, current version: '.PHP_VERSION);}
 require DIR.'vendor/autoload.php';
+define('DEBUG',((new \TMTRU\Configuration())->isDebuggable() && (isset($_GET['debug']) && $_GET['debug']==1)));
+ini_set('display_errors',DEBUG);
+if(version_compare(PHP_VERSION,'8.1.0')<0){die('Required at least PHP version 8.1.0, current version: '.PHP_VERSION);}
