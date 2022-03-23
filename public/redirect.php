3ae2ed88179eeffea95cec8d42f0695f23816400
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-03-23 14:46:48 +0100

    Add Redirect page

---
+++ public/redirect.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Link
+ *
+ * @package TMTRU
+ * @author Manuel Zavatta <manuel.zavatta@gmail.com>
+ */
+
+use TMTRU\Configuration;
+use TMTRU\Link;
+
+require_once('../bootstrap.inc.php');
+
+try{
+	$linkUid=$_REQUEST['link']??'';
+	$Configuration=new Configuration();
+	$Link=new Link($linkUid);
+	$Link->incrementClicks();
+}catch(Exception $Exception){
+	if(DEBUG){
+		var_dump($_REQUEST);
+		var_dump($Configuration);
+		var_dump($Link);
+		//var_dump($Exception);
+		throw $Exception;
+		die();
+	}
+	exit(header('Location: notfound.php'));
+}
+
+?>
+<html>
+	<head>
+		<title>tmtru</title>
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+	</head>
+	<body>
+		<div>
+			<img src="logo.png" alt="coordinator-engine-logo" width="126">
+			<h1>tmtru</h1>
+			<p>Redirecting to <strong><a href="<?php echo $Link->getURL(); ?>"><?php echo $Link->getURL(); ?></a></strong></p>
+			<p id="dots">.</p>
+			<p id="alert" class="hidden alert italic">if redirect doesn't work click the link</p>
+		</div>
+	</body>
+</html>
+<style>
+	div{padding-top:99px;text-align:center;}
+	h1{font-family:sans-serif;font-size:27px;}
+	p{font-family:sans-serif;font-size:18px;}
+	.hidden{visibility:hidden;}
+  .alert{color:orange}
+  .italic{font-style:italic;}
+</style>
+<script>
+  setInterval(function(){
+    const dots=document.getElementById("dots");
+    dots.textContent=dots.textContent+'.';
+  },1000);
+  setTimeout(function(){
+    //window.location.href='<?php echo $Link->getURL(); ?>';
+  },4000);
+  setTimeout(function(){
+    document.getElementById("alert").classList.remove("hidden");
+  },9000);
+</script>
