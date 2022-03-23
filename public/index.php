9d6f7e8edb413460717444d293516ed9ba20ae8e
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-03-23 09:33:52 +0100

    Add public directory, index, favicon, logo and robots

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>tmtru</title>
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+	</head>
+	<body>
+		<div>
+			<img src="logo.png" alt="coordinator-engine-logo" width="126">
+			<h1>tmtru</h1>
+			<p class="italic">"tell me the real url"</p>
+		</div>
+	</body>
+</html>
+<style>
+	div{width:270px;margin:0 auto;padding-top:99px;text-align:center;}
+	h1{font-family:sans-serif;font-size:27px;}
+	p{font-family:sans-serif;font-size:18px;}
+	.italic{font-style:italic;}
+</style>
