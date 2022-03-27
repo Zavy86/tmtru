b442f78ddc48f152c9719474eac70b899c5558d8
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-03-27 15:17:42 +0200

    Fix errors display

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -6,6 +6,7 @@
  * @author Manuel Zavatta <manuel.zavatta@gmail.com>
  */
 error_reporting(E_ALL);
+ini_set('display_errors',true);
 define('DIR',str_replace(['/','\\'],DIRECTORY_SEPARATOR,__DIR__.'/'));
 require DIR.'vendor/autoload.php';
 define('DEBUG',((new \TMTRU\Configuration())->isDebuggable() && (isset($_GET['debug']) && $_GET['debug']==1)));
